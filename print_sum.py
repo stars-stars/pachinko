6bc156d1c4761e916fd6f199dba32c5f95db5cb7
star <yuse.1121hukuoka@gmail.com>
2024-01-24 21:00:35 +0900

    add datas

--- print_sum.py
+++ print_sum.py
@@ -1,4 +1,7 @@
 from bs4 import BeautifulSoup
+import re
+import datetime
+
 
 # 現在の収支を調べる
 soup = BeautifulSoup(open("days.html", encoding="utf-8"), "html.parser")
@@ -21,6 +24,19 @@ returns = [float(temp.replace("k", "")) for temp in returns]
 # 今までの収支を調べる
 soup = BeautifulSoup(open("sum.html", encoding="utf-8"), "html.parser")
 past = soup.find("h1")
-past = past.text
-print(f"今までの総合収支: {past}k")
-print(f"現在の総合収支: {sum(returns) - sum(pays)}k")
+past = past.text.replace("!", "").replace("k", "")
+pastday = soup.find("h3")
+pastday = re.search(r"\d+/\d+/\d+", pastday.text)
+today = str(datetime.date.today())
+today = today.replace("-", "/")
+print(f"{pastday.group()}までの総合収支: {past}k")
+print(f"{today}現在の総合収支: {sum(returns) - sum(pays)}k")
+
+pastday = datetime.datetime.strptime(pastday.group(), "%Y/%m/%d")
+today = datetime.datetime.strptime(today, "%Y/%m/%d")
+
+diff_day = re.search(r"\d+ days", str(today - pastday)).group()
+diff_day = str(diff_day).replace(" days", "")
+diff_money = sum(returns)-sum(pays) - float(past)
+diff_money = "+" + str(diff_money) if diff_money > 0 else str(diff_money)
+print(f"{diff_day}日間で収支{diff_money}k")
