fd7619b13952bd97dddada7eb36645b7fdd656d8
star <yuse.1121hukuoka@gmail.com>
2025-10-02 19:49:46 +0900

    fix js bug

--- update_data.js
+++ update_data.js
@@ -10,50 +10,71 @@ document.addEventListener('DOMContentLoaded', () => {
         })
         .then(data => {
             // 合計収支の埋め込み
-            const totalProfit = data.total_profit;
+            const totalProfit = data.total_profit / 1000;
             // 収支をカンマ区切りにし、[+]または[-]を付けて表示
-            const profitText = new Intl.NumberFormat('ja-JP', {style: 'currency', currency: 'JPY', minimumFractionDigits: 0}).format(totalProfit);
-            document.getElementById('total-profit').textContent = profitText;
+            const profitText = new Intl.NumberFormat('ja-JP', {style: 'decimal', minimumFractionDigits: 1, maximumFractionDigits: 1}).format(totalProfit);
+            document.getElementById('total-profit').textContent = `${profitText}k!!!`;
 
             // 日付の埋め込み
             const today = new Date();
-            document.getElementById('today').textContent = `${today}更新`;
+            const dateOnly = today.toLocaleDateString('ja-JP');
+            document.getElementById('today').textContent = `${dateOnly}更新`;
 
 
             // 機種別データの埋め込み
-            const machineProfit = data.machine_profits;
+            const machineProfits = data.machine_profits;
             const machineTableBody= document.querySelector('#machine-summary tbody');
             // JSONデータをループ処理
-            for (const machine in machineProfit) {
-                const profit = machineProfit[machine];
+            for (const machine in machineProfits) {
+                const name = machineProfits['machine'];
+                const investment = machineProfits['investment'];
+                const returned = machineProfits['return'];
+                const profit = machineProfits['profit'] / 1000;
                 const row = machineTableBody.insertRow();
 
-                // 年月セル
+                // 機種名セル
                 let cellMonth = row.insertCell();
-                cellMonth.textContent = machine;
-
+                cellMonth.textContent = name;
+                // 収支セル
+                let cellInvestment = row.insertCell();
+                const machineInvestmentText = new Intl.NumberFormat('ja-JP', { style: 'decimal', minimumFractionDigits: 1, maximumFractionDigits: 1 }).format(investment);
+                cellInvestment.textContent = `${machineInvestmentText}k`;
+                // 収支セル
+                let cellReturn = row.insertCell();
+                const machineReturnText = new Intl.NumberFormat('ja-JP', { style: 'decimal', minimumFractionDigits: 1, maximumFractionDigits: 1 }).format(returned);
+                cellReturn.textContent = `${machineReturnText}k`;
                 // 収支セル
                 let cellProfit = row.insertCell();
-                const machineProfitText = new Intl.NumberFormat('ja-JP', { style: 'currency', currency: 'JPY', minimumFractionDigits: 0 }).format(profit);
-                cellProfit.textContent = machineProfitText;
+                const machineProfitText = new Intl.NumberFormat('ja-JP', { style: 'decimal', minimumFractionDigits: 1, maximumFractionDigits: 1 }).format(profit);
+                cellProfit.textContent = `${machineProfitText}k`;
             }
 
             // 月別データの埋め込み
-            const monthlyProfit = data.monthly_profits;
+            const monthlyProfits = data.monthly_profits;
             const monthlyTableBody = document.querySelector('#monthly-summary tbody');
             // JSONデータをループ処理
-            for(const month in monthlyProfit){
-                const profit = monthlyProfit[month];
+            for(const month in monthlyProfits){
+                const month = monthlyProfits['month'];
+                const investment = monthlyProfits['investment'];
+                const returned = monthlyProfits['return'];
+                const profit = monthlyProfits['profit'] / 1000;
                 const row = monthlyTableBody.insertRow();
 
                 // 年月セル
                 let cellMonth = row.insertCell();
                 cellMonth.textContent = month.replace('-', '年') + '月';
-
+                // 収支セル
+                let cellInvestment = row.insertCell();
+                const monthlyInvestmentText = new Intl.NumberFormat('ja-JP', { style: 'decimal', minimumFractionDigits: 1, maximumFractionDigits: 1 }).format(investment);
+                cellInvestment.textContent = `${monthlyInvestmentText}k`;
+                // 収支セル
+                let cellReturn = row.insertCell();
+                const monthlyReturnText = new Intl.NumberFormat('ja-JP', { style: 'decimal', minimumFractionDigits: 1, maximumFractionDigits: 1 }).format(returned);
+                cellReturn.textContent = `${monthlyReturnText}k`;
                 // 収支セル
                 let cellProfit = row.insertCell();
-                const monthlyProfitText = new Intl.NumberFormat('ja-JP', {style: 'currency', currency: 'JPY', minimumFractionDigits: 0}).format(profit);
-                cellProfit.textContent = monthlyProfitText;
+                const monthlyProfitText = new Intl.NumberFormat('ja-JP', { style: 'decimal', minimumFractionDigits: 1, maximumFractionDigits: 1 }).format(profit);
+                cellProfit.textContent = `${monthlyProfitText}k`;
             }
         })
         .catch(e => {
