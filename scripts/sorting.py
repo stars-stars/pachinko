bb89930496df4e7ef40bb057b1595c1f0f2dcac9
star <yuse.1121hukuoka@gmail.com>
2025-10-21 00:33:06 +0900

    add function delete duplication rows

--- scripts/sorting.py
+++ scripts/sorting.py
@@ -7,6 +7,9 @@ df = pd.read_csv(filepath)
 # 'date' 列を日付型に変換します (ソートを正確に行うため)
 df['date'] = pd.to_datetime(df['date'])
 
+# 重複する行を削除
+df = df.drop_duplicates()
+
 # 'date' 列を基準に昇順 (古い日付から新しい日付へ) に並び替えます
 df_sorted = df.sort_values(by=['date', 'machine_name'], ascending=[True, True])
 
